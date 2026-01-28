2faefb23e4728c53f53f8e1fd7aa35ed74b36b93
romanusbett <bettromanus@gmail.com>
2026-01-28 22:16:43 +0300

    initial commit

---
+++ src/main/java/com/vision/vision_xi/VisionXiApplication.java
@@ -0,0 +1,13 @@
+package com.vision.vision_xi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisionXiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisionXiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vision/vision_xi/VisionXiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vision.vision_xi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisionXiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
